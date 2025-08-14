3bb1b574fbf7f2987bc29477204fa2d0accfac31
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-08-14 10:28:41 +0800

    feat: send email verification

--- routes/guestRouter.js
+++ routes/guestRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import loginController from "../controllers/guest/loginController.js";
 import registerController from "../controllers/guest/registerController.js";
 import forgotPasswordController from "../controllers/guest/forgotPasswordController.js";
+import verifyEmailController from "../controllers/guest/verifyEmailController.js";
 
 const { Router } = express;
 const guestRouter = Router();
@@ -14,5 +15,8 @@ guestRouter.post("/login", loginController.login);
 
 // Forgot Password
 guestRouter.get("/forgot-password", forgotPasswordController.forgotPassword);
+
+// Verify Email
+guestRouter.get("/verify-email", verifyEmailController.verifyEmail);
 
 export default guestRouter;
